e35cecb7ecebb9ecea7446b83e5e1b67b73c973d
2Zero1 <Good7219@hotmail.com>
2021-03-26 16:54:40 +0900

    made previewClickHandler in writeSecondPage
    - after checking photo and photoMessage are valid, go to nextpage
    - if are they invalid, show error message

--- src/container/WriteSecondFormContainer.test.jsx
+++ src/container/WriteSecondFormContainer.test.jsx
@@ -6,8 +6,6 @@ import { fireEvent, render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
-import placeholders from '../text/placeholders';
-
 import inputFields from '../fixtures/inputFields';
 
 describe('WriteSecondFormContainer', () => {
@@ -33,6 +31,10 @@ describe('WriteSecondFormContainer', () => {
     },
   ));
 
+  beforeEach(() => {
+    dispatch.mockClear();
+  });
+
   it('show title and recommendation message', () => {
     const {
       getByText,
@@ -42,9 +44,64 @@ describe('WriteSecondFormContainer', () => {
     expect(getByText('세로로 된 사진을 사용하시는걸 권장합니다.')).not.toBeNull();
 
     expect(getByLabelText('사진 메시지')).not.toBeNull();
-    expect(getByLabelText('사진 메시지').placeholder).toBe('10 ~ 20자');
+    expect(getByLabelText('사진 메시지').placeholder).toBe('10 ~ 30자');
 
     expect(getByText('미리보기')).not.toBeNull();
+  });
+
+  context('when photo image or photoMessage field is not empty', () => {
+    beforeEach(() => {
+      handleNextClick.mockClear();
+
+      useSelector.mockImplementation((selector) => selector(
+        {
+          inputFields: {
+            ...inputFields,
+            write: {
+              ...inputFields.write,
+              photo: {
+                ...inputFields.write.photo,
+                value: 'fake image url',
+              },
+              photoMessage: {
+                ...inputFields.write.photoMessage,
+                value: 'hello, nice meet you !',
+              },
+            },
+          },
+        },
+      ));
+    });
+
+    it("call handleNextClick", () => {
+      const {
+        getByText,
+      } = renderSecondPage();
+
+      fireEvent.click(getByText('미리보기'));
+      expect(handleNextClick).toBeCalled();
+    });
+  });
+
+  context('when photo image or photoMessage field is empty', () => {
+    beforeEach(() => {
+      handleNextClick.mockClear();
+
+      useSelector.mockImplementation((selector) => selector(
+        {
+          inputFields,
+        },
+      ));
+    });
+
+    it("do not call handleNextClick", () => {
+      const {
+        getByText,
+      } = renderSecondPage();
+
+      fireEvent.click(getByText('미리보기'));
+      expect(handleNextClick).not.toBeCalled();
+    });
   });
 
 });
