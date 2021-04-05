51815f01b363bbe25fe45e6f8160ebdfaca8670e
2Zero1 <Good7219@hotmail.com>
2021-04-05 10:40:43 +0900

    Added NotFoundPage and console in App.jsx
    to check router works well

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,17 @@ import EntrancePage from './page/EntrancePage';
 import WritePage from './page/WritePage';
 import PostcardPage from './page/PostcardPage';
 import TestPage from './page/TestPage';
+import NotFoundPage from './page/NotFoundPage';
 
 export default function App() {
+  console.log('라우팅');
   return (
     <Switch>
       <Route exact path="/write/:index" component={WritePage} />
       <Route exact path="/postcard" component={PostcardPage} />
       <Route exact path="/:key" component={EntrancePage} />
       <Route exact path="/" component={TestPage} />
-
-      {/* <Route component={NotFoundPage} /> */}
+      <Route component={NotFoundPage} />
     </Switch>
   );
 }
