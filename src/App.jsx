745eec285cd5e79c77a2bb10d5e251985bae7552
2Zero1 <Good7219@hotmail.com>
2021-04-05 10:29:24 +0900

    Added TestPage to test github action

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
 import EntrancePage from './page/EntrancePage';
 import WritePage from './page/WritePage';
 import PostcardPage from './page/PostcardPage';
+import TestPage from './page/TestPage';
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
       <Route exact path="/write/:index" component={WritePage} />
       <Route exact path="/postcard" component={PostcardPage} />
       <Route exact path="/:key" component={EntrancePage} />
+      <Route exact path="/" component={TestPage} />
 
       {/* <Route component={NotFoundPage} /> */}
     </Switch>
