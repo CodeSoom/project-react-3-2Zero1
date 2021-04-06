227d704d374c67b1740ee3998aec405b216a46aa
2Zero1 <Good7219@hotmail.com>
2021-04-06 16:34:15 +0900

    Apply temporary design at secondPage of WritePage

--- src/presentational/ContentsForm.jsx
+++ src/presentational/ContentsForm.jsx
@@ -1,5 +1,46 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
+const Button = styled.button(() => ({
+  fontSize: '16px',
+  margin: '10px',
+  padding: '5px 8px',
+}));
+
+const Title = styled.span(() => ({
+  position: 'absolute',
+  top: '10px',
+  left: '50%',
+  textAlign: 'center',
+  fontSize: '20px',
+  transform: 'translateX(-50%)',
+}));
+
+const CompleteButton = styled.button(() => ({
+  position: 'absolute',
+  right: '10px',
+  top: '10px',
+  padding: '5px 8px',
+}));
+
+const Error = styled.p(() => ({
+  marginTop: '2px',
+  fontSize: '12px',
+  color: 'red',
+  marginLeft: '20px',
+}));
+
+const Contents = styled.textarea(({ error }) => ({
+  display: 'block',
+  width: '90%',
+  height: '80%',
+  margin: '10px auto',
+  padding: '10px',
+  fontSize: '20px',
+  borderColor: error ? 'red' : '',
+}));
+
 export default function ContentsForm({
   contents: {
     value,
@@ -21,25 +62,26 @@ export default function ContentsForm({
 
   return (
     <>
-      <button
+      <Title>내용 작성</Title>
+      <Button
         type="button"
         onClick={onClickPrevious}
       >
         이전
-      </button>
-      <div>내용 작성</div>
-      <textarea
-        value={value}
-        placeholder={placeholder}
-        onChange={handleChange}
-      />
-      {errorMessage ? <div>{errorMessage}</div> : null}
-      <button
+      </Button>
+      <CompleteButton
         type="button"
         onClick={handleClick}
       >
         다음
-      </button>
+      </CompleteButton>
+      <Contents
+        value={value}
+        placeholder={placeholder}
+        onChange={handleChange}
+        error={errorMessage}
+      />
+      {errorMessage ? <Error>{errorMessage}</Error> : null}
     </>
   );
 }
