143f9e0dd67109c4370742fb8dcc83f970b9d092
2Zero1 <Good7219@hotmail.com>
2021-04-16 15:50:33 +0900

    Update NotFoundPage.jsx
    - changed message
    - changed css

--- src/page/NotFoundPage.jsx
+++ src/page/NotFoundPage.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
+export const Text = styled.div({
+  fontSize: '35px',
+  marginTop: '30%',
+  textAlign: 'center',
+});
+
 export default function NotFoundPage() {
   return (
     <>
-      <div>not found</div>
+      <Text>존재하지 않는 엽서입니다.</Text>
     </>
   );
 }
