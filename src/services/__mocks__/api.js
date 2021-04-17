e8a7656a1564db6658269e31c354176a01fdccdd
2Zero1 <Good7219@hotmail.com>
2021-04-18 01:04:30 +0900

    set to request postcards api
    - added loadPostcards, setPostcards
    - added fetchPostcards and mock

--- src/services/__mocks__/api.js
+++ src/services/__mocks__/api.js
@@ -10,3 +10,7 @@ export const postPostcard = jest.fn();
 export const postCheckValidPostcard = jest.fn();
 
 export const fetchPostcard = jest.fn();
+
+export async function fetchPostcards() {
+  return { data: [] };
+}
