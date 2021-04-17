bd32fc1d2a0ebf0b26c56a9da4688838a187494c
2Zero1 <Good7219@hotmail.com>
2021-04-17 16:45:34 +0900

    Update Postcard Front
    - added postage stamp instead of photo on the stamp
    - updated stamp image size

--- src/presentational/Postcard.jsx
+++ src/presentational/Postcard.jsx
@@ -39,7 +39,7 @@ export default function Postcard({
             sender={sender}
             receiver={receiver}
             contents={contents}
-            stampUrl={photoUrl}
+            stampUrl="https://postcard-yh1.s3.ap-northeast-2.amazonaws.com/uploads/1618645093193_%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA+2021-04-17+%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE+4.37.12.png"
           />
         )
           : (
