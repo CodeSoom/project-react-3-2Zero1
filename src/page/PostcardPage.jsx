b091b960b7aab371522412640a1da234abcdd014
2Zero1 <Good7219@hotmail.com>
2021-04-17 23:10:20 +0900

    Add test case in PostcardPage
    - changed importing react-router to react-router-dom
    - test goBack is called when previous button is clicked

--- src/page/PostcardPage.jsx
+++ src/page/PostcardPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router';
+import { useHistory } from 'react-router-dom';
 import PostcardContainer from '../container/PostcardContainer';
 
 import { setPostcardFront } from '../state/slice';
