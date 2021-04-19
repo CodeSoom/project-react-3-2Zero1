d1242830707e2b26e6ddc305e66f5af7cc6eafb3
2Zero1 <Good7219@hotmail.com>
2021-04-19 12:00:53 +0900

    Apply Postcards page design

--- src/container/PostcardsContainer.jsx
+++ src/container/PostcardsContainer.jsx
@@ -1,8 +1,31 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+
+import styled from '@emotion/styled';
+
 import PostcardItem from '../presentational/PostcardItem';
 
 import { loadPostcards } from '../state/slice';
+
+import {
+  PreviousButton,
+  DefaultLayout,
+} from '../style/commonCss';
+
+const Title = styled.span(() => ({
+  position: 'absolute',
+  top: '10px',
+  left: '50%',
+  textAlign: 'center',
+  fontSize: '20px',
+  transform: 'translateX(-50%)',
+}));
+
+const List = styled.ul({
+  height: '93%',
+  overflow: 'scroll',
+  textAlign: 'center',
+});
 
 export default function PostcardsContainer({ handlePreviousClick }) {
   const dispatch = useDispatch();
@@ -16,17 +39,17 @@ export default function PostcardsContainer({ handlePreviousClick }) {
   }, []);
 
   return (
-    <>
-      <button
+    <DefaultLayout>
+      <PreviousButton
         type="button"
         onClick={handlePreviousClick}
       >
         이전
-      </button>
-      <div>엽서 모음</div>
-      <ul>
+      </PreviousButton>
+      <Title>엽서 모음</Title>
+      <List>
         {postcards.map((postcard) => (<PostcardItem key={postcard.rid} postcardItem={postcard} />))}
-      </ul>
-    </>
+      </List>
+    </DefaultLayout>
   );
 }
--- src/presentational/PostcardItem.jsx
+++ src/presentational/PostcardItem.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
+const Image = styled.img({
+  width: '80%',
+});
+
+const Item = styled.li({
+  margin: '30px 0',
+});
+
+const From = styled.div({
+  fontSize: '18px',
+  fontWeight: 'bold',
+});
+
+const PhotoMessage = styled.div({
+  fontSize: '12px',
+});
+
 export default function PostcardItem({
   postcardItem: {
     receiver,
@@ -8,10 +27,10 @@ export default function PostcardItem({
   },
 }) {
   return (
-    <li>
-      <div>{`${receiver}님이 받은 엽서`}</div>
-      <img src={photo} alt="postcardImage" />
-      <div>{photoMessage}</div>
-    </li>
+    <Item>
+      <From>{`${receiver}님이 받은 엽서`}</From>
+      <Image src={photo} alt="postcardImage" />
+      <PhotoMessage>{photoMessage}</PhotoMessage>
+    </Item>
   );
 }
