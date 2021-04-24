82b5593b6ecf45f5a07ed4c8ee4c27839588ab2f
2Zero1 <Good7219@hotmail.com>
2021-04-25 01:08:07 +0900

    Move '이전' button to PostcardFront from Postcard
    - '이전' button is showed only in PostcardFront page

--- src/presentational/Postcard.jsx
+++ src/presentational/Postcard.jsx
@@ -5,7 +5,6 @@ import PostcardBack from './PostcardBack';
 
 import {
   DefaultLayout,
-  PreviousButton,
 } from '../style/commonCss';
 
 export default function Postcard({
@@ -24,12 +23,6 @@ export default function Postcard({
 }) {
   return (
     <DefaultLayout>
-      <PreviousButton
-        type="button"
-        onClick={onHandlePreviousClick}
-      >
-        이전
-      </PreviousButton>
       {
         isFrontPage ? (
           <PostcardFront
@@ -37,6 +30,7 @@ export default function Postcard({
             receiver={receiver}
             contents={contents}
             stampUrl="https://postcard-yh1.s3.ap-northeast-2.amazonaws.com/uploads/1618645093193_%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA+2021-04-17+%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE+4.37.12.png"
+            onHandleClickPrevious={onHandlePreviousClick}
             onHandleClickGoToBack={onHandleClickPage}
           />
         )
