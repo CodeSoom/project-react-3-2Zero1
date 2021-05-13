338e72a10b47804bac8005b1aa3d1cf97743bc78
2Zero1 <Good7219@hotmail.com>
2021-05-13 13:40:49 +0900

    Update Test code
    - test what about when previous button is clicked

--- src/page/ExpirePage.test.jsx
+++ src/page/ExpirePage.test.jsx
@@ -37,31 +37,10 @@ describe('ExpirePage', () => {
   it('renders expire page', () => {
     const { getByText, getByPlaceholderText, getByLabelText } = renderExpirePage();
 
-    expect(getByText('파기하기')).not.toBeNull();
-
     expect(getByText('이전')).not.toBeNull();
 
     fireEvent.click(getByText('이전'));
 
     expect(mockGoBack).toBeCalled();
-
-    expect(getByText('발신자님으로 부터 받은 소중한 엽서를 파기하시겠습니까?')).not.toBeNull();
-
-    expect(getByText('엽서 암호를 입력해주세요')).not.toBeNull();
-
-    expect(getByPlaceholderText('5 ~ 20자')).not.toBeNull();
-
-    fireEvent.change(getByLabelText('엽서 암호'), { target: { value: 'hello' } });
-
-    expect(dispatch).toBeCalledWith({
-      type: 'application/changeInputFieldValue',
-      payload: {
-        page: 'expire',
-        type: 'secretMessage',
-        value: 'hello',
-      },
-    });
-
-    expect(getByText('파기')).not.toBeNull();
   });
 });
